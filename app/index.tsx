bf238e5959e118f4e56818a899f2853a33d6e6a1
Olakunle Hassan <kunlex_042003@yahoo.com>
2024-08-27 13:21:05 +0100

    made some adjustment to the app index

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import React from "react";
+import Logo from "../assets/images/easyserve.png";
 
 const App = () => {
   return (
-    <View className="bg-red-500 flex flex-1 justify-center items-center">
-      <Text className="text-white text-2xl">EasyServe</Text>
-    </View>
+    <>
+      <View className="bg-primary flex justify-center items-center h-full">
+        <Image source={Logo} resizeMode="contain" className="w-1/2" />
+        <Text className="font-bold text-2xl text-white">EasyServe</Text>
+      </View>
+    </>
   );
 };
 
