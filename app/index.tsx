34c61462f81eb2d5108b0d7701d65fdc9e40a6a4
Eseka Chinedu <esekachinedu256@gmail.com>
2024-08-27 03:24:10 +0100

    Initial project setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="bg-red-500 flex flex-1 justify-center items-center">
+      <Text className="text-white text-2xl">EasyServe</Text>
+    </View>
+  );
+};
+
+export default App;
